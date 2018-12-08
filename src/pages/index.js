90988c9ef4dc739cec39e83803879b163b891f1b
wabdsgn <wab2com@gmail.com>
2018-12-08 22:10:01 +0100

    init agenda

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,44 @@
 import React from 'react'
-import { Link } from 'gatsby'
+// import { Link } from 'gatsby';
+import moment from 'moment';
 
-import Layout from '../components/layout'
-import Image from '../components/image'
+// import Layout from '../components/layout'
+// import Image from '../components/image'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
+const IndexPage = ({data}) => {
+  const events = data.allWordpressWpEvent.edges.filter(({node : {acf}}) => moment(acf.event_start_date).isSameOrAfter(moment()));
+  return (
+    <div>
+      <h1>Agenda</h1>
+      <ol>
+        {events.map(({node}, index) => {
+          return (
+            <li key={index}>
+              <h2>{node.title}</h2>
+              <p>{moment(node.acf.event_start_date).format('D/MM/YYYY')}</p>
+            </li>
+          );
+        })}
+      </ol>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  );
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query {
+    allWordpressWpEvent {
+      edges {
+        node {
+          id
+          title
+          acf {
+            event_start_date
+            event_end_date
+          }
+        }
+      }
+    }
+  }
+`;
