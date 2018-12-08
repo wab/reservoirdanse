9016427b8d58b5361559c5d6e32eb835ae178947
wabdsgn <wab2com@gmail.com>
2018-12-08 22:09:42 +0100

    use acf

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
         hostingWPCOM: false,
         // If useACF is true, then the source plugin will try to import the WordPress ACF Plugin contents.
         // This feature is untested for sites hosted on WordPress.com
-        useACF: false,
+        useACF: true,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
