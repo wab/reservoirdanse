ee396f440c496dcfd38fb5993d5759cd1eaf3122
wabdsgn <wab2com@gmail.com>
2018-12-11 19:10:35 +0100

    query pages init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,46 @@
 import React from 'react'
-// import { Link } from 'gatsby';
+import { Link } from 'gatsby';
 import moment from 'moment';
 
 // import Layout from '../components/layout'
 // import Image from '../components/image'
 
+const PAGES_ID = {
+  'projet' : 20,
+  'residences' : 1062
+}
+
 const IndexPage = ({data}) => {
   const events = data.allWordpressWpEvent.edges.filter(({node : {acf}}) => moment(acf.event_start_date).isSameOrAfter(moment()));
+
+  const projet = data.allWordpressPage.edges.filter(({node}) => node.wordpress_id === PAGES_ID['projet'])[0].node;
+
+  const residences = data.allWordpressPage.edges.filter(({node}) => node.wordpress_id === PAGES_ID['residences'])[0].node;
   return (
     <div>
-      <h1>Agenda</h1>
+      <ul>
+        <li>
+          <Link to={projet.slug}>
+            <article>
+              <h2>{projet.title}</h2>
+              <div dangerouslySetInnerHTML={{__html: projet.excerpt }}></div>
+            </article>
+          </Link>
+        </li>
+        <li>
+          <Link to={residences.slug}>
+            <article>
+              <h2>{residences.title}</h2>
+              <div dangerouslySetInnerHTML={{__html: residences.excerpt }}></div>
+            </article>
+          </Link>
+        </li>
+        <li>Les résidences (page + événements de la catégorie En résidence et Ouverture Publique)</li>
+        <li>Le Monstre (page + événements de la catégorie Le Monstre)</li>
+        <li>L’espace ressource (page)</li>
+        <li>Actualité (page + défilement des actualités de la semaine ou de 3 événements)</li>
+      </ul>
+      <p>Création des catégories d’événements pour les actualités : En résidence • Ouverture publique • Le Monstre • Stage / Atelier • Ressource • Divers</p>
       <ol>
         {events.map(({node}, index) => {
           return (
@@ -37,6 +68,17 @@ export const query = graphql`
             event_start_date
             event_end_date
           }
+        }
+      }
+    },
+    allWordpressPage {
+      edges {
+        node {
+          id
+          title
+          excerpt
+          slug
+          wordpress_id
         }
       }
     }
